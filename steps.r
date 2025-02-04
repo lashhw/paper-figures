0daffca782752e3b65fae5809d7b29eb96f69091
Yen-Chieh Huang <lashhw81@gmail.com>
2025-02-04 13:30:42 +0800

    .

--- steps.r
+++ steps.r
@@ -3,15 +3,20 @@ library(extrafont)
 library(ggpattern)
 
 data <- tribble(
-                             ~category,      ~type,     ~KIT,       ~BA,     ~BMW,      ~CLA,     ~HOU,      ~STR,     ~TEA,
-       "(a) Ray-Box Intersection Test", "Baseline", 80039307,  93736200, 73315482, 121983935, 45775577,  92641215, 64172522,
-       "(a) Ray-Box Intersection Test",    "AQB48", 87278088, 101420034, 76223221, 136460270, 48822114, 103765710, 67435411,
-  "(b) Ray-Triangle Intersection Test", "Baseline", 12292964,  14845355, 23956184,  16331386,  9744976,  25946748, 21797483,
-  "(b) Ray-Triangle Intersection Test",    "AQB48", 20533796,  21241906, 28976705,  31165607, 11751613,  33948321, 25236623,
+                             ~category,      ~type,     ~KIT,      ~BA,     ~BMW,     ~CLA,     ~HOU,     ~STR,    ~TEA,
+       "(a) Ray-Box Intersection Test", "Baseline", 27350466, 32281744, 25391961, 34350719, 22064428, 51227574, 8819512,
+       "(a) Ray-Box Intersection Test", "Compress", 27800813, 32689835, 25805727, 34722533, 22461394, 51650496, 9085097,
+       "(a) Ray-Box Intersection Test",    "AQB48", 28852894, 34148649, 26358502, 35590843, 23226071, 52830697, 9203416,
+  "(b) Ray-Triangle Intersection Test", "Baseline",  4141323,  5016284,  7231499,  4069250,  4436902, 14501956, 3106594,
+  "(b) Ray-Triangle Intersection Test", "Compress",  4224343,  5145595,  7301799,  4168922,  4510828, 14674073, 3131536,
+  "(b) Ray-Triangle Intersection Test",    "AQB48",  5277028,  6086967,  8150865,  5346766,  5132850, 15635343, 3290746,
 )
 
 data_long <- data |>
   pivot_longer(cols=!c(category, type), names_to="scene", values_to="value") |>
+  group_by(scene) |>
+  mutate(value=value/value[category == "(a) Ray-Box Intersection Test" & type == "Baseline"]) |>
+  ungroup() |>
   print()
 
 data_long_mean <- data_long |>
@@ -22,22 +27,15 @@ data_long_mean <- data_long |>
 
 data_long_combined <- bind_rows(data_long, data_long_mean) |>
   mutate(
-    value_norm=value/max(value),
     category=factor(category, levels=unique(category)),
     type=factor(type, levels=unique(type)),
     scene=factor(scene, levels=unique(scene))
   ) |>
-  select(!value) |>
-  print()
-
-data_increment <- data_long_combined |>
-  pivot_wider(names_from=type, values_from=value_norm) |>
-  mutate(ratio=(AQB48-Baseline)/AQB48) |>
   print()
 
 fig <- ggplot(data_long_combined) +
   geom_col_pattern(
-    aes(x=scene, y=value_norm, fill=type, pattern=type),
+    aes(x=scene, y=value, fill=type, pattern=type),
     position="dodge",
     color="black",
     width=0.75,
@@ -46,29 +44,20 @@ fig <- ggplot(data_long_combined) +
     pattern_spacing=0.06,
     pattern_color="#765541"
   ) +
-  geom_text(
-    data=data_increment,
-    aes(x=scene, y=AQB48+max(AQB48)*0.05, label=sprintf("+%.0f%%", ratio*100)),
-    color="grey20",
-    size=3,
-    family="Noto Serif",
-    hjust=0.3,
-  ) +
   facet_wrap(~category) +
   labs(
     x="Scenes",
-    y="Normalized Operation\nCounts"
+    y="Normalized\nOperation Counts"
   ) +
   scale_pattern_manual(
-    values=c("Baseline"="none", "AQB48"="stripe"),
+    values=c("Baseline"="none", "Compress"="stripe", "AQB48"="crosshatch"),
     guide="none"
   ) +
   scale_fill_manual(
-    values=c("Baseline"="#dccbc0", "AQB48"="#b8947f"),
-    labels=c("Baseline"="Baseline-2", "AQB48"="AQB48-2"),
+    values=c("Baseline"="#dccbc0", "Compress"="#b8947f", "AQB48"="#a6795e"),
+    labels=c("Baseline"="Baseline-2", "Compress"="Compress-2", "AQB48"="AQB48-2"),
     guide=guide_legend(title=NULL)
   ) +
-  scale_y_continuous(expand=expansion(mult=c(0, 0.05))) +
   theme_minimal(base_family="Noto Serif") +
   theme(
     legend.position="top",
@@ -82,4 +71,4 @@ fig <- ggplot(data_long_combined) +
     panel.spacing.x=unit(0, "cm"),
     panel.spacing.y=unit(0.65, "cm")
   )
-ggsave("steps.pdf", width=7, height=3.5)
+ggsave("steps.pdf", width=7, height=2.8)
